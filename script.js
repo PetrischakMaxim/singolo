4274417fca52707375fd5faa046f8643a3a7768c
Maxim Petischak <petrischak.maxim@gmail.com>
2020-03-15 21:03:47 +0200

    feat: create slider functions & add slider images

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 /* Header */
-
 function linksScrollAfterClick() {
   const pageLinks = document.querySelectorAll(".navigation__item a");
 
@@ -8,6 +7,54 @@ function linksScrollAfterClick() {
       evt.preventDefault();
       const currentTarget = document.querySelector(this.getAttribute("href"));
       currentTarget.scrollIntoView();
+    });
+  });
+}
+
+/* Slider. Переключение слайдов */
+function sliderMove() {
+  let currentSlide = 0;
+  let visibleSliderClass = "slider__slide--visible";
+  const slider = document.querySelector(".slider");
+  const sliderItem = slider.querySelectorAll(".slider__slide");
+  const sliderItemLenght = sliderItem.length;
+  const sliderButtons = slider.querySelectorAll(".slider__btn");
+
+  sliderButtons.forEach(button => {
+    button.addEventListener("click", function() {
+      const currentDirection = this.dataset.direction;
+
+      if (currentDirection === "next") {
+        if (currentSlide == sliderItemLenght - 1) {
+          sliderItem[currentSlide].classList.remove(visibleSliderClass);
+          currentSlide = 0;
+          sliderItem[currentSlide].classList.add(visibleSliderClass);
+          return;
+        }
+        sliderItem[currentSlide].classList.remove(visibleSliderClass);
+        currentSlide++;
+        sliderItem[currentSlide].classList.add(visibleSliderClass);
+      } else {
+        if (!currentSlide) {
+          sliderItem[currentSlide].classList.remove(visibleSliderClass);
+          currentSlide = sliderItemLenght - 1;
+          sliderItem[currentSlide].classList.add(visibleSliderClass);
+          return;
+        }
+        sliderItem[currentSlide].classList.remove(visibleSliderClass);
+        currentSlide--;
+        sliderItem[currentSlide].classList.add(visibleSliderClass);
+      }
+    });
+  });
+}
+/* Slider. Активация экранов телефонов */
+function switchPhoneScreen() {
+  const phoneZone = document.querySelectorAll(".slider__zone");
+  phoneZone.forEach(phone => {
+    const phoneButton = phone.querySelector(".slider__phone-button");
+    phoneButton.addEventListener("click", function() {
+      phone.classList.toggle("slider__zone--off");
     });
   });
 }
@@ -37,4 +84,6 @@ function toggleStateForPortfolio() {
 document.addEventListener("DOMContentLoaded", () => {
   linksScrollAfterClick();
   toggleStateForPortfolio();
+  sliderMove();
+  switchPhoneScreen();
 });
